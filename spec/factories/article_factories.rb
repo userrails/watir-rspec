71cf8d3998b90a854cc36c5e3bdebfab37bfbf39
rails <rails@rails>
2014-12-01 16:37:19 +0530

    make uniqueness valition for the rspec test

--- spec/factories/article_factories.rb
+++ spec/factories/article_factories.rb
@@ -1,15 +1,6 @@
 FactoryGirl.define do
-  # factory :article do |f|
-  #   f.id 1
-  #   f.name "ruby material"
-  #   f.address "Bommanhalli"
-  #   f.created_at Time.now
-  #   f.updated_at Time.now
-  # end
-
-
   factory :article do
-    name "article name"
-    address "article address"
+    name "name"
+    address "address"
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,21 +1,42 @@
 require 'spec_helper'
 
 describe Article do
+  # test for presence true for rspec 2.0 syntax
    it "validation fails when all fields nil" do
     # below is the rspec 2.0 syntax
     article1 = FactoryGirl.build(:article, name: nil, address: nil)
     article1.should_not be_valid
   end
 
+  # test for prsence true for rspec 2.0 syntax
   it "valids when fields are not blank" do
     # below is the rspec 2.0 syntax
-    article2 = FactoryGirl.build(:article, name: "shiv", address: "address 1")
+    article2 = FactoryGirl.build(:article, name: "article1", address: "address 1")
     article2.should be_valid
   end
   
-  it "Validates all fields presence when presence true" do
+  # test for presence true for rspec 3.0 syntax
+  it "validates all fields presence when presence true" do
     article3 = FactoryGirl.build(:article)
     # below is the rspec 3.0 syntax
     expect(article3).to be_valid
   end
+
+  # make validation fails for uniqueness for the article name, this will through the error with validation not passed
+  it "validation pass when article name is unique" do
+    article4 = FactoryGirl.build(:article, name: "article1")
+    p "-------------------------------------------"
+    p article4.name
+    p "-------------------------------------------"
+    Article.create(:name => article4.name)
+    article4.should_not be_valid
+  end
+
+  # make validation pass for uniqueness for the article name
+  it "validation pass when article name are different" do 
+    article5 = FactoryGirl.build(:article, name: "article1")
+    Article.create(:name => "article2")
+    article5.should be_valid
+  end
+
 end

ff416a33acc4a5a65c095917cfb8106686fbfcaa
rails <rails@rails>
2014-12-01 16:05:05 +0530

    rspec model test for article pass with various syntax in rspec 2.0 and  rspec 3.0

--- spec/factories/article_factories.rb
+++ spec/factories/article_factories.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
-  factory :article do |f|
-    f.name "ruby material"
-    f.address "Bommanhalli"
+  # factory :article do |f|
+  #   f.id 1
+  #   f.name "ruby material"
+  #   f.address "Bommanhalli"
+  #   f.created_at Time.now
+  #   f.updated_at Time.now
+  # end
+
+
+  factory :article do
+    name "article name"
+    address "article address"
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,9 +1,22 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Article, :type => :model do
+describe Article do
+   it "validation fails when all fields nil" do
+    article1 = FactoryGirl.build(:article, name: nil, address: nil)
+    article1.should_not be_valid
+  end
 
-  it "all fields are mandatory" do
-    siv = Article.create!(name: 'siv rj', address: 'bangalore')
-    prz = Article.create!(name: 'prz', address: 'bangalore')
+  it "valids when fields are not blank" do
+    article2 = FactoryGirl.build(:article, name: "shiv", address: "address 1")
+    article2.should be_valid
+  end
+  
+  it "Validates all fields presence when presence true" do
+    article3 = FactoryGirl.build(:article)
+    # below is the rspec 2.0 syntax
+    # article.should be_valid
+
+    # below is the rspec 3.0 syntax
+    expect(article3).to be_valid
   end
 end

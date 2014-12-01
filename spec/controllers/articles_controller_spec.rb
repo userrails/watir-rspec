b04e6dd8ea3b1bd9b5dcbeaa156b96f52ece4f0f
rails <rails@rails>
2014-12-01 17:39:35 +0530

    tested rspec controller index action with FactoryGirl article resource for index action

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,7 +2,17 @@ require 'spec_helper'
 
 describe ArticlesController do
   before(:each) do
-    article1 = FactoryGirl.create(:article, name: "article1", address: "address1")
-    article2 = FactoryGirl.create(:article, name: "article2", address: "address2")
+    @article1 = FactoryGirl.create(:article, name: "article1", address: "address1")
+    @article2 = FactoryGirl.create(:article, name: "article2", address: "address2")
   end
+
+  # rspec for index action
+  it "it should pass if index calls and loads all articles" do 
+    Article.all.should == [@article1, @article2]
+    get :index, {:params1 => "0123"}
+    response.should be_success
+  end
+
+  
+
 end

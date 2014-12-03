00f352aabae6bcac151bfba393c44bde2d743c2b
rails <rails@rails>
2014-12-03 16:11:35 +0530

    small changes

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -8,13 +8,11 @@ describe "localhost articles index" do
   let(:address) {SecureRandom.hex(10)}
   before(:each) { browser.goto "http://localhost:3000" } 
 
-  after(:all) {browser.close}
+  # after(:all) {browser.close}
 
   it "should click on new link and save the articles and checks if articles present" do
     browser.link(href: '/articles/new').click
     # expect(browser.url).to eq('http://localhost:3000/')
-
-    
 
     browser.text_field(:id, "article_name").when_present.set(name)
     browser.textarea(:id, "article_address").when_present.set(address)   
@@ -27,8 +25,13 @@ describe "localhost articles index" do
    # expect(browser.div(class: 'notice').text).to eq('Article was successfully created.')
   end
 
-  it "should click on show link and view the article" do
-    browser.link(href: "/articles/29").click
-  end
+   it "should click on show link and view the article" do
+     browser.link(href: "/articles/40").click
+   end
+
+   # it "should click on edit link and go to the articles edit page" do
+   #   browser.link(href: "/articles/40/edit").click
+   #   browser.button.click
+   # end
   
 end

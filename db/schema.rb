3ba6a4c3e41b1124ece4388e9f96e0b183569c14
rails <rails@rails>
2015-05-15 18:13:03 +0530

    Write condition for the cucumber testing for posts only

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209101206) do
+ActiveRecord::Schema.define(version: 20150515122952) do
 
   create_table "articles", force: true do |t|
     t.string   "name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20141209101206) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
